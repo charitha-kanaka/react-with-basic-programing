086f6e4022df7c8be30a526593311ebd13385188
charitha-kanaka <charithakanaka05@gmail.com>
2024-10-03 07:15:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    <div>
+      <Header></Header>
+
+      <main>
+      <h2>Success</h2>
+      </main>
+
+<Footer></Footer>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+//Functional Component with Anonymous Function
+
+
+
+const Footer= function(){
+   
+    <hr/>
+    return <p>copyright 2024 </p>
+}
+export default Footer;
